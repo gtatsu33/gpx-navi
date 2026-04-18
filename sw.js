782e4bc44c20bdba40b16b184894e64852cb6bf3
Tatsumi Sakaguchi <gtatsu33@gmail.com>
2026-04-18 17:02:31 +0900

    v2

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // GPX ナビ Service Worker
 // アプリシェルとマップタイルをキャッシュしてオフライン動作を実現する
 
-const SHELL_CACHE = 'gpxnav-shell-v1';
+const SHELL_CACHE = 'gpxnav-shell-v2';
 const TILE_CACHE  = 'gpxnav-tiles-v1';
 
 const SHELL_ASSETS = [
