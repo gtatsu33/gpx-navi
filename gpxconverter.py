0c75faada50b14d871d3d0fd9a8e0c573bf73342
Tatsumi Sakaguchi <gtatsu33@gmail.com>
2026-06-07 11:02:45 +0900

    fix overlapped wpt2

--- gpxconverter.py
+++ gpxconverter.py
@@ -22,7 +22,7 @@ from rdp import rdp as rdp_simplify
 import plotly.graph_objects as go
 import xml.etree.ElementTree as ET
 
-APP_VERSION = "3.1.0"
+APP_VERSION = "3.2.0"
 
 _GPX_NS      = "http://www.topografix.com/GPX/1/1"
 _GPXNAVI_NS  = "https://gpxnavi"
